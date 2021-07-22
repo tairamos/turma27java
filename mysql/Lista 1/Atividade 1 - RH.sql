8e38d196505e7b742c0d52681732c3775424bfcc
tairamos <tairamos.dev@gmail.com>
2021-07-22 11:45:43 -0300

    Att mysql

--- mysql/Lista 1/Atividade 1 - RH.sql
+++ mysql/Lista 1/Atividade 1 - RH.sql
@@ -1,2 +1,22 @@
 
-select * from tb_funcionarios where salario < 2000;
+/*create database db_rh_informacoes;*/
+
+/*use db_rh_informacoes;*/
+
+/*Create table tb_funcionarios(
+	id bigint (10) auto_increment,
+	nome varchar (20) not null,
+    salario decimal (6,0) not null,
+    cargo varchar (15),
+    setor decimal (2)
+);*/
+
+/*Insert into tb_funcionarios (nome, salario, funcao, setor) Values ("Camila", 1900, "Recrutadora", "01");
+insert into tb_funcionarios (nome, salario, funcao, setor) Values ("João", 3700, "Desenvolvedor", "04");
+insert into tb_funcionarios (nome, salario, funcao, setor) Values ("Fernando", 1100, "Assistente", "02");
+insert into tb_funcionarios (nome, salario, funcao, setor) Values ("Joana", 1200, "Estagiaria", "02");
+insert into tb_funcionarios (nome, salario, funcao, setor) Values ("Maria", 4000, "Supervisora", "05");*/
+
+/*select * from tb_funcionarios where salario > 2000;
+select * from tb_funcionarios where salario < 2000;*/
+
